7eea6859cf651242d37c41db58b90a278ad55c1e
jocwsh <jojolyn.wal@gmail.com>
2025-04-26 02:13:24 +1000

    Created character, camera, camera movement and crosshair

---
+++ GAM20001 Assignment 3 (shooter)/Assets/Scripts/cameraView.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class cameraView : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        yRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+
+    }
+}
